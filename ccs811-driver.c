d9da3dca66e35beb0293344c3dde5c064dd86445
Théo Stelmaszyk <theo.svk@gmail.com>
2024-01-16 20:28:40 +0100

    Première version d'un code fonctionnel qui test uniquement le registre hardware
    Signed-off-by: Théo Stelmaszyk <theo.svk@gmail.com>

--- ccs811-driver.c
+++ ccs811-driver.c
@@ -7,7 +7,7 @@
 const uint LED_PIN = 25;
 const uint NWAKE_PIN = 0;
 
-static uint8_t ADDRESS = 0x5A; //When ADDR is low the 7 bit I2C address is 0x5A (if high --> 0x5B)
+static uint8_t ADDRESS = 0x5A; 
 const uint8_t REG_STATUS = 0x00;
 const uint8_t REG_HW_ID = 0x20;
 
@@ -16,7 +16,6 @@ int main() {
     bi_decl(bi_program_description("This is a test binary."));
     bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
     bi_decl(bi_1pin_with_name(NWAKE_PIN, "On-board for nwake"));
-
     stdio_init_all();
 
     gpio_init(LED_PIN);
@@ -24,8 +23,8 @@ int main() {
     gpio_init(NWAKE_PIN);
     gpio_set_dir(NWAKE_PIN, GPIO_OUT);
 
-    //uint8_t buf[32];
-    unsigned char buf[32];
+    uint8_t buf[32];
+
 
     // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
     i2c_init(i2c_default, 400 * 1000);
@@ -42,15 +41,12 @@ int main() {
     puts("Hello World\n");
     sleep_ms(1000);
 
-    //gpio_put(NWAKE_PIN, 1);
-    sleep_ms(1);
-    buf[0]=0x20;
-    printf("Valeur envoyeee: %02x\n", buf[0]);
-    //i2c_write_blocking(i2c_default, ADDRESS, &REG_HW_ID, 1, true);
-    i2c_write_blocking(i2c_default, ADDRESS, buf, 1, true);
+    gpio_put(NWAKE_PIN, 0);
+    printf("Valeur envoyeee: %02x\n", &REG_HW_ID);
+    i2c_write_blocking(i2c_default, ADDRESS, &REG_HW_ID, 1, true);
     i2c_read_blocking(i2c_default, ADDRESS, buf, 1, false);
     printf("Valeur lue: %02x\n", buf[0]);
-    //gpio_put(NWAKE_PIN, 0);
+    gpio_put(NWAKE_PIN, 1);
 
     }
 }
